8f7fdb406ad1d6f6f43cf4a047d2737daa7d6734
Mark May <mamark90@gmail.com>
2020-07-21 17:18:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,7 +70,7 @@ class Student
     end
   end
   
-    def self.first_tudent_in_grade_10
+    def self.first_student_in_grade_10
     sql = <<-SQL
       SELECT * FROM students
       WHERE students.grade = 10
